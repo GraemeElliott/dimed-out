a656db49352f1e6cee0500076041205a019a734f
Graeme Elliott <graeme.elliott@dimedout.com>
2024-03-24 00:48:51 +0000

    Sanity integration has started

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {vercelDeployTool} from 'sanity-plugin-vercel-deploy'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: '4m3hd9h1',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), vercelDeployTool()],
 
   schema: {
     types: schemaTypes,
