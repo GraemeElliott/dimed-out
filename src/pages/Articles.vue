a480d28a28ccaedb90f126f0f75d769cae63f03e
Graeme Elliott <graeme.elliott@dimedout.com>
2024-04-13 01:12:33 +0100

    Updated Articles  page

--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue';
 import { sanityClient } from '@/client.ts';
 import imageUrlBuilder from '@sanity/image-url';
-import { Article, FetchedArticle } from '@/types/types';
+import { Article } from '@/types/types';
 
 // Create an instance of the urlBuilder
 const builder = imageUrlBuilder(sanityClient);
@@ -14,8 +14,8 @@ const fetchArticles = () => {
     '*[_type == "article"]{ _id, title, "articleImage": articleImage }';
   sanityClient
     .fetch(query)
-    .then((data: FetchedArticle[]) => {
-      articles.value = data.map((article: FetchedArticle) => {
+    .then((data: Article[]) => {
+      articles.value = data.map((article: Article) => {
         const articleImageUrl =
           article.articleImage && typeof article.articleImage === 'object'
             ? builder.image(article.articleImage).url()
